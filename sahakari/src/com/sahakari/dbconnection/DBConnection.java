826f5282550aa386c083eafe87a5d5e8709c7e72
shishir <shishir4913120@gmail.com>
2018-01-05 10:40:55 +0545

    #final push

--- sahakari/src/com/sahakari/dbconnection/DBConnection.java
+++ sahakari/src/com/sahakari/dbconnection/DBConnection.java
@@ -15,7 +15,7 @@ public class DBConnection {
     }
     public static Connection getConnection(){
         try{
-            con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/coop_dat","root","centoa");//javproject name of database
+            con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/coop_dat","root","centos");//javproject name of database
         }
         catch(Exception e){
             e.printStackTrace();
