456d67c57dc9410b9a1a0ae792ce30be29d001ed
sunil <suniltandukar50@gmail.com>
2018-02-19 14:12:32 +0545

    puu

--- sahakari/src/com/sahakari/daoimpl/LoanDaoImpl.java
+++ sahakari/src/com/sahakari/daoimpl/LoanDaoImpl.java
@@ -23,13 +23,15 @@ public class LoanDaoImpl implements LoanDao{
 			ps.setString(5,am.getStartDateEn() );
 			ps.setString(6,am.getMaturityDateNP() );
 			ps.setString(7,am.getMaturityDateEn() );
-			ps.setString(8,am.getRepaymentFrequency() );
-			ps.setString(9,am.getLoanDrawdownAc() );
-			ps.setString(10,am.getChargeDebitAc() );
-			ps.setString(11,am.getChargeAmount() );
-			ps.setString(12,am.getChargePaymentDate() );
-			ps.setString(13,am.getInterestLiqAccount() );
-			ps.setString(14, am.getAccountNumber());
+			ps.setString(8, am.getRepaymentStartDateNp());
+			ps.setString(9,am.getRepaymentStartDateEn());
+			ps.setString(10,am.getRepaymentFrequency() );
+			ps.setString(11,am.getLoanDrawdownAc() );
+			ps.setString(12,am.getChargeDebitAc() );
+			ps.setString(13,am.getChargeAmount() );
+			ps.setString(14,am.getChargePaymentDate() );
+			ps.setString(15,am.getInterestLiqAccount() );
+			ps.setString(16, am.getAccountNumber());
 			int i=ps.executeUpdate();
 			if(i>0){
 				return true;
