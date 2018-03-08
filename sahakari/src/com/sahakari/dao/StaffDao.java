3a8930dba4a5773af54aa54d6680010cea94a753
Sunil <Sunil@DESKTOP-K3SG9MG>
2018-03-08 17:45:05 +0545

    hh

--- sahakari/src/com/sahakari/dao/StaffDao.java
+++ sahakari/src/com/sahakari/dao/StaffDao.java
@@ -1,9 +1,13 @@
 package com.sahakari.dao;
 
+import java.util.List;
+
 import com.sahakari.model.StaffModel;
 
 public interface StaffDao {
 
 	public boolean insertStaff(StaffModel s);
+
+	public List<StaffModel> getStaff();
 
 }
---
+++ sahakari/src/com/sahakari/model/TellerModel.java
@@ -0,0 +1,66 @@
+package com.sahakari.model;
+
+public class TellerModel {
+	
+	private String userId;
+	
+	private String tellerId;
+	
+	private String accountName;
+	
+	private String accountNumber;
+	
+	private String openDate;
+	
+	private String openDateN;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getTellerId() {
+		return tellerId;
+	}
+
+	public void setTellerId(String tellerId) {
+		this.tellerId = tellerId;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getOpenDate() {
+		return openDate;
+	}
+
+	public void setOpenDate(String openDate) {
+		this.openDate = openDate;
+	}
+
+	public String getOpenDateN() {
+		return openDateN;
+	}
+
+	public void setOpenDateN(String openDateN) {
+		this.openDateN = openDateN;
+	}
+	
+
+}
